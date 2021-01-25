648bfed45c7dbda0f7fe8807b95c6640b976cda5
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-01-25 18:32:53 -0400

    agregado de cdn para elaborar el mapa

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const form = document.getElementById('form');
-
 form.addEventListener('submit', getdata)
 
+
 async function getdata(e){
+	
     e.preventDefault()
 
    const data = new FormData(form)
 
-   const response = await fetch('vista.php',{
+   const response = await fetch('Backend.php',{
        method: 'post',
        body: data,
    })
 
    const get = await response.json()
    console.log(get)
+
 }
+
