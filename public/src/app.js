11e32485a28b1917086d042c78524f83eab51603
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-02-12 09:38:06 -0400

    agregado del mapa por defecto

--- public/src/app.js
+++ public/src/app.js
@@ -3,6 +3,7 @@ import Ui from './ui.js'
 import Mapa from './map.js'
 
 
+
 const fetch = new Fetch
  
 const form = document.getElementById('form');
@@ -27,3 +28,29 @@ async function Initapp(){
 
 }
 
+
+let map = L.map('map')
+map.setView([51.505, -0.09], 13);
+ setInterval(validar_inicializado_mapa, 3000)
+//titlelayer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     }).addTo(map);
+       
+    //marker
+     L.marker([51.505, -0.09]).addTo(map)
+    .bindPopup('Estás aquí')
+    .openPopup();
+
+
+
+
+function validar_inicializado_mapa(){
+
+let container = L.DomUtil.get('map');
+    //condición que evalua si el mapa fue inicializado
+    if(container != null){
+       
+       container._leaflet_id = null;
+    }
+
+}
--- public/src/map.js
+++ public/src/map.js
@@ -1,29 +1,17 @@
 export default function Mapa(ip){
    
-    let map = L.map('map').setView([ip.latitude, ip.longitude], 13);
-    
-    setInterval(validar_inicializado_mapa, 3000)
-    
-    //titlelayer
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     }).addTo(map);
-       
+   const mapa = L.map('map').setView([ip.latitude, ip.longitude], 13);
+   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     }).addTo(mapa);
+           
     //marker
-     L.marker([ip.latitude, ip.longitude]).addTo(map)
-    .bindPopup('Estás aquí')
+     L.marker([ip.latitude, ip.longitude])
+    .bindPopup('Estás aquí').addTo(mapa)
     .openPopup();
       
 }
 
 
-function validar_inicializado_mapa(){
 
-let container = L.DomUtil.get('map');
-    //condición que evalua si el mapa fue inicializado
-    if(container != null){
-       
-       container._leaflet_id = null;
-    }
 
-}
 
