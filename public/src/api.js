52ea12d3ae4dbb5eba7865b6c2bde372c499db9a
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-02-13 18:22:24 -0400

    update

--- public/src/api.js
+++ public/src/api.js
@@ -2,7 +2,7 @@ export default class IpApi {
     async api() {
         const ip = document.getElementById("ip").value;
 
-        const response = await fetch("http://ipwhois.app/json/" + ip);
+        const response = await fetch("https://ipwhois.app/json/" + ip);
 
         let get = await response.json();
         return get;
