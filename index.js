e9b4e51aa1830dc6357b8e51217825ebb21a2472
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2021-02-13 12:55:45 -0400

    acabado de la app

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
 
 //configuracion
-app.set('port', process.env.PORT|| 3000)
+app.set("port", process.env.PORT || 3000);
 
 //archivos estaticos
-app.use(express.static(__dirname + '/public'))
-
+app.use(express.static(__dirname + "/public"));
 
 //escuchando el servidor
-app.listen(app.get('port'), ()=>{
-	console.log('server en el puerto:' + app.get('port'));
-})
+app.listen(app.get("port"), () => {
+    console.log("server en el puerto:" + app.get("port"));
+});
